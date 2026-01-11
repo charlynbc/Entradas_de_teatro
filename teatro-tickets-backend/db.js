e79f2eea6dcfdf8a6c82b2c81aecbe839f8b867c
Charly Barrios <111667268+charlynbc@users.noreply.github.com>
2026-01-11 23:25:36 +0000

    🧹 Limpieza: Eliminar 50+ archivos obsoletos (tests viejos, scripts deprecados, migraciones antiguas)

--- teatro-tickets-backend/db.js
+++
@@ -1,5 +0,0 @@
-// db.js - Exporta funciones de dataStore.js para persistencia en archivo JSON
-import dataStore, * as dataStoreFns from './utils/dataStore.js';
-
-export * from './utils/dataStore.js';
-export default dataStore;
--- tests/test-funcionalidades.js
+++
@@ -1,3 +0,0 @@
-console.log('ℹ️  tests/test-funcionalidades.js está obsoleto (era del stack Mongo/Mongoose).');
-console.log('ℹ️  Usá la suite actual: node tests/run-all.js');
-process.exit(0);
